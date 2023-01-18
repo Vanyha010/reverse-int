2d47f309c4536c90292edd21b805868c69e4a769
KotyaLapkin <kotyalapkin@yahoo.com>
2023-01-18 21:05:44 +0300

    I DID IT

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let digits = Math.abs(n)
+  let str = String(digits);
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    result = `${str[i]}${result}`
+  }
+  let finalResult = Number(result);
+  return finalResult;
 }
